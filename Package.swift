19f8fe35695aaff5692ae205eabd8a76491da66e
Glenn Posadas <hello@glennvon.com>
2022-05-04 11:18:15 +0800

    update path assets

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             dependencies: [],
             path: "CRNotifications",
             resources: [
-              .process("CRNotificationsMedia")
+              .process("CRNotifications/Assets")
             ]
         )
     ],
